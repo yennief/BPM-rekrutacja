d8e52857797fe1618c369e17871b753e54c46c2d
Justyna Frączek <justynkaf553@gmail.com>
2023-12-22 14:00:48 +0100

    init project

---
+++ src/main/java/org/bp/RekrutacjaApplication.java
@@ -0,0 +1,9 @@
+package org.bp;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Job interview Application")
+public class RekrutacjaApplication extends ServletProcessApplication {
+
+}
